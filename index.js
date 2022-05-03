2c76b333b438476264ed51a6b99c2a5539ba8073
Svetlana <Kostosv585@yandex.ru>
2022-05-03 10:33:29 +0300

    main

---
+++ index.js
@@ -0,0 +1,14 @@
+// const colors = require('colors');
+import colors from 'colors';
+// console.log(process.argv);
+// const [ name1, name2 ] = process.argv.slice(2);
+
+// const args = process.argv.slice(2);
+// console.log(args);
+// const [ name1, name2 ] = args;
+
+// const name1 = args[0];
+// const name2 = args[1];
+// console.log('Hello Node.js to ' + name1 + ' and ' + name2);
+
+console.log(colors.green('Hello World!'));
