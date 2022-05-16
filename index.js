18e419ebd1d9ad11c15067d55fe80547fce52eca
Svetlana <Kostosv585@yandex.ru>
2022-05-16 14:14:05 +0300

    hw-2

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
-// const colors = require('colors');
-import colors from 'colors';
-// console.log(process.argv);
-// const [ name1, name2 ] = process.argv.slice(2);
-
-// const args = process.argv.slice(2);
-// console.log(args);
-// const [ name1, name2 ] = args;
-
-// const name1 = args[0];
-// const name2 = args[1];
-// console.log('Hello Node.js to ' + name1 + ' and ' + name2);
-
-//console.log(colors.green('Hello World!'));
+import EventEmitter from 'events'
+const emitter = new EventEmitter()
+import 'moment-precise-range-plugin'
+import moment from 'moment'
 
 
+const [userPastDate] = process.argv.slice(2)
+const format = 'YYYY-MM-DD HH'
 
-let mainCount = 0
-let [startMain, endMain] = process.argv.slice(2)
-const colorsPallete = [colors.green, colors.yellow, colors.red]
-
-if (!startMain || !endMain) {
-	console.log(colors.red('Простых чисел в диапазоне нет...Введите числа вида от 0 до 100'))
-} else {
-	if (startMain < 2) {
-		startMain = 2
-	}
-for (let i = startMain; i <= endMain; i++) {
-	let isMain = true
-
-	for (let j = 2; j < i; j++) {
-		if (i % j === 0) {
-			isMain  = false
-		}
-	}
-
-		if (isMain) {
-			console.log(colorsPallete[mainCount % 3](i))
-			mainCount++
-		}
-	}
-
-	  if (!mainCount) {
-		console.log(colors.red('Простых чисел в диапазоне не было найдено'))
-	  }
+const getDateFromDateString = dateLine => {
+	const newLocal =  dateLine.split('-')
+	const [hour, day, month, year] = newLocal
+	return new Date(Date.UTC(year, month - 1, day, hour))
 }
+
+const futureDate = getDateFromDateString(userPastDate)
+const showRemainingTime = futureDate => {
+	const dateNow = new Date()
+	if (dateNow >= futureDate) {
+		emitter.emit('timerDone')
+	} else {
+		const futureDateFormatted = moment(futureDate, format)
+		const currentDateFormatted = moment(dateNow, format)
+		const diff = preciseDiff(futureDateFormatted, currentDateFormatted)
+
+		console.log(diff)
+	}
+}
+
+const timerId = setInterval(() => {
+	emitter.emit('timerTick', futureDate)
+}, 1009)
+
+const showTimerDone = timerId => {
+	clearInterval(timerId)
+	console.log('End')
+}
+
+emitter.on('timerTick', showRemainingTime)
+emitter.on('timerDone', () => {
+	showTimerDone(timerId)
+})
